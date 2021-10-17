aaa245c40c15d211fd7662af041165cda4ce1eae
kvid1908 <41445525+kvid1908@users.noreply.github.com>
2021-10-17 21:29:48 +0530

    Add files via upload

---
+++ src/main/java/com/org/intrestcalculator/IntrestCalculationApplication/IntrestCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.org.intrestcalculator.IntrestCalculationApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrestCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrestCalculationApplication.class, args);
+	}
+
+}
